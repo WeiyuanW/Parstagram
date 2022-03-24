43c1cf7a5ae03ab1e1196e54791a36a69ba62051
Weiyuan Wu <66765006+WeiyuanW@users.noreply.github.com>
2022-03-24 17:40:33 -0400

    Parstagram assignment part 1

---
+++ Parstagram/APIKeys.swift
@@ -0,0 +1,10 @@
+//
+//  APIKeys.swift
+//  Parstagram
+//
+//  Created by 伍伟源 on 3/23/22.
+//
+
+import Foundation
+let APP_ID = "DopFeA87wukh5UmkZp5GFpofeTSJyRFIPoTvNAC1"
+let CLIENT_KEY = "Iqxeyomjb8GnAJy0IhDNUMMDTW6Hy7nj1u1bT0EZ"
--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 伍伟源 on 3/23/22.
 //
 
+import Parse
 import UIKit
 
 @main
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = APP_ID // <- UPDATE
+                $0.clientKey = CLIENT_KEY // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
